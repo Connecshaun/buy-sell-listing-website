1965c3ba00074424ac8f195d5ab81cf5a3d15217
Jorawer Grewal <joegrewal20@gmail.com>
2022-02-04 23:15:58 -0800

    Fix minor eslint errors and fix description value in newListing.ejs

--- routes/favourites.js
+++ routes/favourites.js
@@ -11,12 +11,12 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const cookieID = req.session["users_id"];
-    console.log(cookieID)
+    console.log(cookieID);
     db.query(`SELECT beverages.name, beverages.description, beverages.price, beverages.thumbnail_url, beverages.posted_at FROM favourites JOIN users ON users.id = user_id JOIN beverages ON beverages.id = beverage_id WHERE users.id = ${cookieID};`)
       .then(data => {
         const beverages = data.rows;
-        console.log(beverages)
-        const templateVars = {beverages}
+        console.log(beverages);
+        const templateVars = {beverages};
         res.render("favourites", templateVars);
       })
       .catch(err => {
