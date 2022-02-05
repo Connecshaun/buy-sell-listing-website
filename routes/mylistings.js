9a49bb27be7d9485fcd8773d5d20004b8a31f97b
Shaun Robinson <connecshaun@gmail.com>
2022-02-05 04:30:20 +0000

    Setup route for mylistings along with ejs file

---
+++ routes/mylistings.js
@@ -0,0 +1,44 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const cookieID = req.session["users_id"];
+    console.log(cookieID)
+    db.query(`SELECT beverages.name, beverages.description, beverages.price, beverages.thumbnail_url, beverages.posted_at FROM beverages JOIN users ON users.id = user_id WHERE users.id = ${cookieID};`)
+      .then(data => {
+        const beverages = data.rows;
+        console.log(beverages)
+        const templateVars = {beverages}
+        res.render("mylistings", templateVars);
+      })
+      .catch(err => {
+        console.log("YOLO")
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
+
+
+// .then(data => {
+//   const users = data.rows;
+//   res.json({ users });
+// })
+// .catch(err => {
+//   res
+//     .status(500)
+//     .json({ error: err.message });
+// });
+// });
+// return router;
+// };
