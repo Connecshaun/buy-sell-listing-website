5e0569b1accc2e3c3cfed54951337f011b2a254c
Jorawer Grewal <joegrewal20@gmail.com>
2022-02-10 01:25:44 -0800

    Reroute categories.js into separate files/routes

---
+++ routes/sourBeers.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const {beveragesSelected} = require('../public/scripts/helpers');
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const cookieID = req.session["users_id"];
+    db.query(
+      `SELECT beverages.id, name, description, price, thumbnail_url, posted_at, beverages.is_available, sold_at, seller_id FROM beverages JOIN categories ON category_id = categories.id WHERE categories.type = 'Sour Beers';`
+    )
+      .then((data) => {
+        const beverages = data.rows;
+        console.log("beverages:", beverages, "beveragesSelected:", beveragesSelected);
+        const templateVars = { beverages, cookieID, beveragesSelected };
+        res.render("sourBeers", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
