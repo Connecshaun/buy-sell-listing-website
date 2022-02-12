daafd31599c468b62fed83f4f80fee772ceddef6
Shaun Robinson <connecshaun@gmail.com>
2022-02-12 08:46:25 +0000

    Playing

---
+++ public/scripts/alert.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $( #clickme ).click(function() {
+      $( ".pop_up_copy" ).slideToggle( "slow", function() {
+        // Animation complete.
+      });
+
+  // alert("Hello World! Welcome to Tutorialdeep.")
+  })
+}
